c037f890085f9818c9dd89b5244046efefb278bf
kannaradam <adamkannar2008@gmail.com>
2025-12-03 15:05:43 +0100

    Add project files.

---
+++ Hazi_xUnit/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Xunit;
+using System;
+
+namespace teszteles
+{
+    public class Szamolo
+    {
+        public int Duplaz(int szam)
+        {
+            return szam * 2;
+        }
+    }
+}
+
+
+namespace teszteles.Tests
+{
+    public class SzamoloTeszt
+    {
+        [Fact] 
+        public void Duplaz_NegyetKap_NyolcatAdVissza()
+        {
+            var szamolo = new Szamolo(); 
+            int bemenet = 4;
+            int elvart = 8;
+
+ 
+            int tenyleges = szamolo.Duplaz(bemenet); 
+
+           
+            Assert.Equal(elvart, tenyleges); 
+
+        }
+    }
+}
